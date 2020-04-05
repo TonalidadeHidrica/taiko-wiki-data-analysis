1393e58ab6709c6258140ef5fd2e6cbd20e4a5ed
TonalidadeHidrica <47710717+TonalidadeHidrica@users.noreply.github.com>
2020-04-06 02:40:31 +0900

    Made a separate function to get the file list.

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,64 @@
 extern crate reqwest;
 
 use scraper::{Html, Selector};
+use std::error;
 
 const URL_PREFIX: &str = "http://www.wikihouse.com/taiko/";
 
+#[derive(Debug)]
+struct WikiFileEntry {
+    title: String,
+    last_update: String,
+    file_name: String,
+}
+
 #[tokio::main]
-async fn main() -> Result<(), reqwest::Error> {
-    let html: String = reqwest::Client::new()
-        .get(URL_PREFIX)
-        .query(&[("cmd", "filelist")])
-        .send().await?
-        .text().await?;
-    let document = Html::parse_document(&html);
+async fn main() -> Result<(), Box<dyn error::Error>> {
+    let reqwest_client = reqwest::Client::new();
 
-    for element in document.select(&Selector::parse("div.container div.body > ul > li > ul > li").unwrap()) {
-        let a_element = element.select(&Selector::parse("a").unwrap()).next().unwrap();
-        let small_element = element.select(&Selector::parse("small").unwrap()).next().unwrap();
-        let li_element = element.select(&Selector::parse("ul > li").unwrap()).next().unwrap();
-
-        let title = a_element.text().collect::<Vec<_>>().join("");
-        let last_update = small_element.text().collect::<Vec<_>>().join("");
-        let file_name = li_element.text().collect::<Vec<_>>().join("");
-
-        println!("{}\t{}\t{}", title, last_update, file_name);
-    }
+    get_file_list(reqwest_client).await?;
 
     Ok(())
+}
+
+async fn get_file_list(
+    reqwest_client: reqwest::Client,
+) -> Result<Vec<WikiFileEntry>, reqwest::Error> {
+    let html: String = reqwest_client
+        .get(URL_PREFIX)
+        .query(&[("cmd", "filelist")])
+        .send()
+        .await?
+        .text()
+        .await?;
+    let document = Html::parse_document(&html);
+
+    Ok(document
+        .select(&Selector::parse("div.container div.body > ul > li > ul > li").unwrap())
+        .map(|element| {
+            // let a_element = element.select_first("a");
+            let a_element = element
+                .select(&Selector::parse("a").unwrap())
+                .next()
+                .unwrap();
+            let small_element = element
+                .select(&Selector::parse("small").unwrap())
+                .next()
+                .unwrap();
+            let li_element = element
+                .select(&Selector::parse("ul > li").unwrap())
+                .next()
+                .unwrap();
+
+            let title = a_element.text().collect::<Vec<_>>().join("");
+            let last_update = small_element.text().collect::<Vec<_>>().join("");
+            let file_name = li_element.text().collect::<Vec<_>>().join("");
+
+            WikiFileEntry {
+                title,
+                last_update,
+                file_name,
+            }
+        })
+        .collect())
 }
