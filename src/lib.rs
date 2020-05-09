ffcb7b781043d033dd4dd19a2d73b9b86b73a5a4
TonalidadeHidrica <47710717+TonalidadeHidrica@users.noreply.github.com>
2020-05-10 01:35:50 +0900

    Added HTML5 table parser.

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,4 @@
+pub mod html5_table_parser;
 pub mod my_itertools;
 pub mod pukiwiki_reparser;
+pub mod utils;
---
+++ src/utils.rs
@@ -0,0 +1,17 @@
+pub fn chmin<T>(input: &mut T, limit: T) -> bool where T: PartialOrd {
+    if *input > limit {
+        *input = limit;
+        true
+    } else {
+        false
+    }
+}
+
+pub fn chmax<T>(input: &mut T, limit: T) -> bool where T: PartialOrd {
+    if *input < limit {
+        *input = limit;
+        true
+    } else {
+        false
+    }
+}
